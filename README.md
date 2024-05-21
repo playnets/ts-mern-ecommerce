a0a83f29a082db09d209845546a6695cc2fb94b2
playnets <booncomart@gmail.com>
2024-05-21 09:47:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TypeScript MERN E-Commerce
+
+## Lesson
+
+1. Introduction
+2. Install Tools
+3. Create TypeScript React By Vite
+4. Create Git Repoistory
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <header>A.Win Shop </header>
+      <main>Content</main>
+      <footer>All right reserved</footer>
+    </>
+  )
+}
+
+export default App
