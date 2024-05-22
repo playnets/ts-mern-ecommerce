8065d9159d3c759483a5fe362aea3309a80c7c22
playnets <booncomart@gmail.com>
2024-05-22 10:16:58 +0700

    List-Product

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 2. Install Tools
 3. Create TypeScript React By Vite
 4. Create Git Repoistory
+5. List Products
+   1. create Product type
+   2. create products array
+   3. add product images
+   4. render products
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
+import { sampleProducts } from "./data";
+import './App.css'
 
 function App() {
-
   return (
     <>
       <header>A.Win Shop </header>
-      <main>Content</main>
+      <main>
+        <ul>
+          {sampleProducts.map((product)=>(
+            <li key={product.slug}>
+              <img src={product.image} alt={product.image} className="product-image" />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
