81b0185099efbc770faee92b2246cbb606621153
playnets <booncomart@gmail.com>
2024-05-22 13:54:27 +0700

    07-Add-Page-Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
-import { sampleProducts } from "./data";
+import { Container, Nav, Navbar, } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -22,19 +22,7 @@ function App() {
 
       <main>
         <Container className="mt-3">
-          <Row>
-              {sampleProducts.map((product) => (
-                <Col key={product.slug} sm={6} md={4} lg={3}>
-                  <img
-                    src={product.image}
-                    alt={product.image}
-                    className="product-image"
-                  />
-                  <h3>{product.name}</h3>
-                  <p>${product.price}</p>
-                </Col>
-              ))}
-          </Row>
+          <Outlet/>
         </Container>
       </main>
 
---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,5 @@
+//rfc
+
+export default function ProductPage() {
+  return <div>ProductPage</div>;
+}
