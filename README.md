2d85df26864676f1d0f78aedd90aaf351a5e8e97
playnets <booncomart@gmail.com>
2024-05-22 11:27:01 +0700

    6. Add React Bootstrap

--- README.md
+++ README.md
@@ -11,4 +11,8 @@
    2. create products array
    3. add product images
    4. render products
+6. Add React Bootstrap
+   1. npm install react-bootstrap bootstrap
+   2. import bootstrap css
+   3. update App.tsx
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,47 @@
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import { sampleProducts } from "./data";
-import './App.css'
 
 function App() {
   return (
-    <>
-      <header>A.Win Shop </header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Aj.Win Shop</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+
       <main>
-        <ul>
-          {sampleProducts.map((product)=>(
-            <li key={product.slug}>
-              <img src={product.image} alt={product.image} className="product-image" />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+              {sampleProducts.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3}>
+                  <img
+                    src={product.image}
+                    alt={product.image}
+                    className="product-image"
+                  />
+                  <h3>{product.name}</h3>
+                  <p>${product.price}</p>
+                </Col>
+              ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All right reserved</footer>
-    </>
+
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
+    </div>
   );
 }
 
