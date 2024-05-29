078b44fda7d23a97391b1b9e57c1953060397125
playnets <booncomart@gmail.com>
2024-05-29 11:19:35 +0700

    Video-15-Connect-To-MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,25 @@
-import express, { Request, Response } from 'express'
-import { sampleProducts } from './data'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import { seedRouter } from './routers/seedRouter'
+import { productRouter } from './routers/productRouter'
+
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb+srv://boonlerts:1234@cluster0.wpx9jgd.mongodb.net/tsmernbbvc?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 
 const app = express()
-
 app.use(
   cors({
     credentials: true,
@@ -11,13 +27,8 @@ app.use(
   })
 )
 
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts)
-})
-
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
-})
+app.use('/api/products', productRouter)
+app.use('/api/seed', seedRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
